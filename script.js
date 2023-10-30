d5aab0a8df18b66720956e6bf27145ebaa1f020d
Gideon Onyegbula <gigi.onyegbula.2007@gmail.com>
2023-10-30 10:49:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-let views = 1;
-while (views <= 10) {
-    console.log(views)
-    views++;
+const button = document.querySelector("button");
+
+function confirmPassword(entry1, entry2) {
+    if (entry1 === entry2) {
+        //All good!//
+    }
+    else {
+        alert("Passwords do not match, input passwords again!")
+    }
 }
-for (let likes = 9; likes >= 1; likes--) {
-    console.log(likes)
+
+function getInfo(argument) {
+    let fname = document.getElementById('firstname').value;
+    let lname = document.getElementById('lastname').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    let pw = document.getElementById('password').value;
+    let cpw = document.getElementById('confirmPassword').value;
+    
+    let userInfo = {
+        fn: fname,
+        ln: lname,
+        e: email,
+        ph: phone,
+        p: pw,
+        cp: cpw
+    }
+    
+    console.log(userInfo);
+    
+    
+    confirmPassword(pw, cpw);
 }
+
+button.onclick = getInfo;
